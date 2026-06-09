18ec5c2aa0adecae6f9a0388c025dfcefcab6704
LugolBis <ld.desmares@gmail.com>
2026-06-09 14:53:48 +0200

    Add URL decoder

--- src/main/scala/producer/Dto.scala
+++ src/main/scala/producer/Dto.scala
@@ -1,6 +1,8 @@
 package producer
 
 import io.circe.{Decoder, HCursor, Json}
+import java.net.URLDecoder
+import java.nio.charset.StandardCharsets
 
 case class MetaData(uri: String, domain: String, dt: String)
 implicit val decoder: Decoder[MetaData] =
@@ -9,5 +11,9 @@ implicit val decoder: Decoder[MetaData] =
       uri <- c.downField("meta").downField("uri").as[String]
       domain <- c.downField("meta").downField("domain").as[String]
       dt <- c.downField("meta").downField("dt").as[String]
-    } yield new MetaData(uri, domain, dt)
+    } yield new MetaData(uri.decodeURL(), domain, dt)
   }
+
+extension (str: String)
+  def decodeURL(): String =
+    URLDecoder.decode(str, StandardCharsets.UTF_8)
