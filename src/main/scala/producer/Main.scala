1a229882d14d3114856926fbd15c74ba7e877ed0
LugolBis <ld.desmares@gmail.com>
2026-06-10 10:01:27 +0200

    Update the entrypoint of the package

--- src/main/scala/producer/Main.scala
+++ src/main/scala/producer/Main.scala
@@ -8,5 +8,9 @@ object Main extends IOApp:
   def run(args: List[String]): IO[ExitCode] =
     for
       ref <- Ref.of[IO, Option[String]](Some("2026-01-01T00:00:00.00Z"))
-      _ <- new Wikimedia[IO](ref, Main.maxRecords).run()
+      _ <- new KafkaWr[IO](
+        "new-page",
+        new Wikimedia[IO](ref, Main.maxRecords),
+        "kafka-broker:29092"
+      ).run()
     yield ExitCode.Success
