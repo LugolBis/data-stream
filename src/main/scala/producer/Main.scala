f6f085f6b40a5daeb822e382acb38874a1fe98e6
LugolBis <ld.desmares@gmail.com>
2026-06-10 10:06:37 +0200

    Remove hardcoded config for the producer entrypoint

--- src/main/scala/producer/Main.scala
+++ src/main/scala/producer/Main.scala
@@ -6,11 +6,16 @@ object Main extends IOApp:
   val maxRecords = 10
 
   def run(args: List[String]): IO[ExitCode] =
+    val bootstrapServers = sys.env.getOrElse(
+      "KAFKA_BOOTSTRAP_SERVERS",
+      "kafka-broker:29092"
+    )
+
     for
       ref <- Ref.of[IO, Option[String]](Some("2026-01-01T00:00:00.00Z"))
       _ <- new KafkaWr[IO](
         "new-page",
         new Wikimedia[IO](ref, Main.maxRecords),
-        "kafka-broker:29092"
+        bootstrapServers
       ).run()
     yield ExitCode.Success
