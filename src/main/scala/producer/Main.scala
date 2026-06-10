f383d1ba00be55c384da393b0ddac50224721605
LugolBis <ld.desmares@gmail.com>
2026-06-10 15:55:13 +0200

    Add the topic size description at the end of the producer execution

--- src/main/scala/producer/Main.scala
+++ src/main/scala/producer/Main.scala
@@ -1,21 +1,19 @@
 package producer
 
 import cats.effect.{ExitCode, IO, IOApp, Ref}
+import admin.Admin
 
 object Main extends IOApp:
   val maxRecords = 10
 
   def run(args: List[String]): IO[ExitCode] =
-    val bootstrapServers = sys.env.getOrElse(
-      "KAFKA_BOOTSTRAP_SERVERS",
-      "kafka-broker:29092"
-    )
-
     for
       ref <- Ref.of[IO, Option[String]](Some("2026-01-01T00:00:00.00Z"))
       _ <- new KafkaWr[IO](
         "new-page",
         new Wikimedia[IO](ref, Main.maxRecords),
-        bootstrapServers
+        "kafka-broker:29092"
       ).run()
+      size <- Admin[IO]("kafka-broker:29092").topicSizeGb("new-page")
+      _ <- IO.println(f"Total size of the topic 'new-page' : $size%.8f GB")
     yield ExitCode.Success
