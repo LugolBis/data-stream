37e5faf2dae11cd6734cbe83a6ece92ec492f0eb
LugolBis <ld.desmares@gmail.com>
2026-06-10 13:41:20 +0200

    Add trait and class for Kafka consumer

---
+++ src/main/scala/consumer/Kafka.scala
@@ -0,0 +1,36 @@
+package consumer
+
+import cats.effect.Async
+import cats.Parallel
+import fs2.io.net.Network
+import fs2.Stream
+import cats.effect._
+import cats.syntax.all._
+import io.circe.syntax._
+import fs2.kafka._
+import fs2.kafka.consumer.KafkaConsumeChunk.CommitNow
+
+trait DtoConsumer[F[_]: Async]:
+  def processRecords(
+      stream: Stream[F, CommittableConsumerRecord[F, String, String]]
+  ): F[Unit]
+
+class KafkaRdr[F[_]: Async: Parallel](
+    topic: String,
+    dtoConsumer: DtoConsumer[F],
+    bootstrapServers: String
+):
+  private val consumerSettings: ConsumerSettings[F, String, String] =
+    ConsumerSettings(
+      keyDeserializer = Deserializer[F, String],
+      valueDeserializer = Deserializer[F, String]
+    ).withAutoOffsetReset(AutoOffsetReset.Earliest)
+      .withBootstrapServers(bootstrapServers)
+
+  def run(): F[Unit] =
+    dtoConsumer.processRecords(
+      KafkaConsumer
+        .stream(consumerSettings)
+        .subscribeTo(topic)
+        .records
+    )
