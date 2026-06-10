f9cc49be0a38984dd3e093fdf90719c362c78bfa
LugolBis <ld.desmares@gmail.com>
2026-06-10 14:09:28 +0200

    Update the entrypoint of the consumer package

--- src/main/scala/consumer/Main.scala
+++ src/main/scala/consumer/Main.scala
@@ -1,5 +1,17 @@
 package consumer
 
-object Main:
-  def main(args: Array[String]): Unit =
-    println("Hello from Consumer!")
+import cats.effect.{ExitCode, IO, IOApp}
+
+object Main extends IOApp:
+  def run(args: List[String]): IO[ExitCode] =
+    val bootstrapServers = sys.env.getOrElse(
+      "KAFKA_BOOTSTRAP_SERVERS",
+      "kafka-broker:29092"
+    )
+
+    for _ <- new KafkaRdr[IO](
+        "new-page",
+        new Console[IO],
+        bootstrapServers
+      ).run()
+    yield ExitCode.Success
